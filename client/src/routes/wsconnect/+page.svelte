df9d27e95d366e5bd925c17651f1a5bab9ba2b99
JakkeLab <itsjakk3@gmail.com>
2024-11-24 00:17:45 +0900

    env setting done

--- client/src/routes/wsconnect/+page.svelte
+++ client/src/routes/wsconnect/+page.svelte
@@ -14,8 +14,30 @@
 <script lang="ts">
     export let websocket: WebSocket | null = null;
 
+    export let logs: {time: Date, message: string}[] = [];
+
     const connectWebsocket = () => {
-        websocket = new WebSocket('ws://localhost:8080');
+        websocket = new WebSocket(`${process.env.VITE_WS_URL}:${process.env.VITE_WS_PORT}`);
+
+        websocket.onopen = () => {
+            const time = new Date();
+            logs = [...logs, { time, message: 'WebSocket connected' }];
+        };
+
+        websocket.onmessage = (event) => {
+            const time = new Date();
+            logs = [...logs, { time, message: `Message received: ${event.data}` }];
+        };
+
+        websocket.onerror = (error) => {
+            const time = new Date();
+            logs = [...logs, { time, message: 'WebSocket error' }];
+        };
+
+        websocket.onclose = () => {
+            const time = new Date();
+            logs = [...logs, { time, message: 'WebSocket disconnected' }];
+        };
     };
 </script>
 
@@ -30,9 +52,20 @@
     <button class="pl-2 pr-2 button-default" onclick={connectWebsocket}>
         Connect Websocket
     </button>
+    <button class="pl-2 pr-2 button-default" onclick={() => logs = []}>
+        Clear Logs
+    </button>
 </div>
 <hr/>
 <div class="flex pl-2 h-[44px] items-center">
     Logs below
 </div>
 <hr/>
+<div class="flex flex-col p-2 h-full border-b" style="height: 600px; overflow-y: auto;">
+    {#each logs as log (log.time)}
+        <div class="log-entry">
+            <span>[{log.time.toLocaleTimeString()}]</span> - <span>{log.message}</span>
+        </div>
+    {/each}
+</div>
+
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	define: {
+		'process.env.VITE_WS_URL': JSON.stringify(process.env.VITE_WS_URL),
+		'process.env.VITE_WS_PORT': JSON.stringify(process.env.VITE_WS_PORT),
+	}
 });
