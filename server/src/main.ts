4f70424544e9c5eb3c2a58fcfdc4c88fa5f7091a
JakkeLab <itsjakk3@gmail.com>
2024-11-24 00:30:24 +0900

    server setting

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,27 +1,41 @@
 import WebSocket from "ws";
+import dotenv from 'dotenv';
 
-const PORT = 8080;
-const wss = new WebSocket.Server({port: PORT}, () => {
-    console.log(`WebSocket server is running on ws://localhost:${PORT}`);
-});
+dotenv.config();
 
-wss.on('connection', (ws) => {
-    console.log('New client connected');
+const PORT = process.env.WS_PORT;
 
-    // 클라이언트로부터 메시지 수신
-    ws.on('message', (message) => {
-        console.log(`Received: ${message}`);
-        // 클라이언트에 메시지 되돌려주기 (에코)
-        ws.send(`Server: ${message}`);
+function runServer() {
+    if(!PORT) {
+        console.error("PORT is not defined.")
+        return;
+    }
+
+    const wss = new WebSocket.Server({port: parseInt(PORT)}, () => {
+        console.log(`WebSocket server is running on ws://localhost:${PORT}`);
     });
-
-    // 클라이언트 연결 종료 처리
-    ws.on('close', () => {
-        console.log('Client disconnected');
+    
+    wss.on('connection', (ws) => {
+        console.log('New client connected');
+    
+        // 클라이언트로부터 메시지 수신
+        ws.on('message', (message) => {
+            console.log(`Received: ${message}`);
+            // 클라이언트에 메시지 되돌려주기 (에코)
+            ws.send(`Server: ${message}`);
+        });
+    
+        // 클라이언트 연결 종료 처리
+        ws.on('close', () => {
+            console.log('Client disconnected');
+        });
     });
-});
+    
+    // 에러 처리
+    wss.on('error', (error) => {
+        console.error(`WebSocket server error: ${error.message}`);
+    });
+}
 
-// 에러 처리
-wss.on('error', (error) => {
-    console.error(`WebSocket server error: ${error.message}`);
-});
+runServer();
+
